e0aa219444a9e5676fbc46a7b8730119d8e56270
Vyacheslav Tretyakov <tretiakovslava@gmail.com>
2021-12-12 17:11:38 +0500

    static impl

--- main.cpp
+++ main.cpp
@@ -5,131 +5,88 @@
 using namespace std;
 
 const int N = 10;
+const double a = 1.0;
+const double h = a / N;
+const double tau = h * h / 2 / 2;
+const double T = 0.5;
+//Находим кол-во точек по шагам времени. Факту - m
+const int Nt = T / tau;
+//лямбда
+const double L = tau / h;
+const double L2 = pow(L, 2);
 
 //функция по варианту
 double func(double x) {
-    if (x >= 0.7)
-        return (9.0 * x - 4.5) / 0.2;
-    if (x < 0.7)
-        return (-9.0 * x + 8.1) / 0.2;
-    return 0.0;
+  if (x <= 0.7 && x >= 0.5)
+    return (9.0 * x - 4.5) / 0.2;
+  if (x > 0.7 && x <= 0.9)
+    return (-9.0 * x + 8.1) / 0.2;
+  return 0.0;
 }
 
 double g(double x) { return 0.0; }
 
-//с методички
+//функция по методичке
 double funcExample(double x) {
-    if (x < 0.5) return 2.0 * x;
-    return 2.0 - 2.0 * x;
+  if (x < 0.5) return 2.0 * x;
+  return 2.0 - 2.0 * x;
 }
 
 //Подпрограмма по методичке
-double *pp(double tau, double h, const double *y1, const double *y2) {
-    //лямбда
-    double L = tau / h;
-    auto *y3 = new double[N + 1];
-    y3[0] = 0;
-    y3[N] = 0;
-    double L2 = pow(L, 2);
-
-    for (int i = 1; i < N; i++) {
-        y3[i] = 2 * (1 - L2) * y2[i] + L2 * (y2[i + 1] - y2[i - 1]) - y1[i];
-    }
-    return y3;
-}
+/*double *pp(double tau, double h, const double *y1, const double *y2) {
+  auto *y3 = new double[N + 1];
+  y3[0] = 0;
+  y3[N] = 0;
+  for (int i = 1; i < N; i++) {
+    //была найдена ошибка -> во втором слагаемом +
+    y3[i] = 2 * (1 - L2) * y2[i] + L2 * (y2[i + 1] - y2[i - 1]) - y1[i];
+  }
+  return y3;
+}*/
 
 int main() {
 
-    ofstream fileOutput = ofstream("output.txt");
-    auto time1 = clock();
+  ofstream fileOutput = ofstream("output.txt");
+  auto time1 = clock();
 
-    double t = 0.0;
-    double a = 1;
+  double u[Nt + 1][N + 1];
 
-    double h = a / N;
-    double tau = h * h / 2 / 2;
-    double T = 1.0;
-    //Находим кол-во точек по шагам в итоге при Т=1 будет 400 точек t
-    auto Nt = T / tau;
+  //начальные и краевые условия
+  u[1][N] = u[1][0] = u[0][N] = u[0][0] = 0.0;
+  for (int i = 1; i < N; i++) {
+    //x от 0 до 1 в i * h
+    u[0][i] = func(i * h); //y1
+    u[1][i] = u[0][i] + tau * g(i); //y2
+  }
 
-    //u0
-    auto *y1 = new double[N + 1];
-    //u1
-    auto *y2 = new double[N + 1];
-
-    auto **u = new double *[(long) Nt];
-
-    //начальные и краевые условия
-    for (int i = 0; i <= N; i++) {
-        auto x = i * h;
-        //x от 0 до 1
-        y1[i] = func(i * h);
-        y2[i] = y1[i] + tau * g(i);
-        //u[i][0] = y1[i];
-        //u[i][1] = y2[i];
+  //Идем по времени от 0 до T. y3 замена функции pp
+  for (int t = 1; t < Nt; t++) {
+    u[t + 1][0] = 0.0;
+    u[t + 1][N] = 0.0;
+    for (int i = 1; i < N; i++) {
+      u[t + 1][i] = 2.0 * (1.0 - L2) * u[t][i] + L2 * (u[t][i + 1] + u[t][i - 1]) - u[t - 1][i];
     }
+  }
 
-    /*auto *y3 = pp(tau, h, y1, y2);
-    t += tau;
-    auto row0 = u[0] = new double[N + 1];
-    for (int i = 0; i <= N; i++) {
-        row0[i] = y3[i];
-    }*/
-    auto *y3 = new double[N + 1];
+  //double time2 = (static_cast<double>(clock()) - time1) / CLOCKS_PER_SEC;
+  //fileOutput << "Time: " << time2 << "\n";
 
-    //Зафиксиурем индекс для матрицы вывода
-    int uIdx = 0;
-    while (t < T) {
+  //выводим координаты x
+  fileOutput << 0 << ",";
+  for (int i = 0; i < N; i++) {
+    fileOutput << i * h << ",";
+  }
+  fileOutput << 1 << "\n";
 
-        //Перекладывание массивов
-        for (int i = 0; i <= N; i++) {
-            y1[i] = y2[i];
-            y2[i] = y3[i];
-        }
-
-        //Вычисление УМФ
-        y3 = pp(tau, h, y1, y2);
-
-        int iterIdx = floor(t / T * Nt);
-        t = t + tau;
-
-        //перекладывем результат в матрицу вывода. По факту u[i][j] = y3[i]
-        if (iterIdx == uIdx || t >= T) {
-            double *row = u[uIdx] = new double[N + 1];
-            for (int i = 0; i <= N; i++) {
-                row[i] = y3[i];
-            }
-            uIdx++;
-        }
+  for (int i = 0; i <= Nt; i++) {
+    //координаты по T
+    fileOutput << i * tau << ",";
+    for (int j = 0; j < N; j++) {
+      fileOutput << u[i][j] << ",";
     }
-
-    double time2 = (static_cast<double>(clock()) - time1) / CLOCKS_PER_SEC;
-    fileOutput << "Time: " << time2 << "\n" << "\n";
-
-
-    double x = 0.0;
-    for (int i = 0; i <= N; i++) {
-        fileOutput << x << ",";
-        x += 0.1;
-    }
+    fileOutput << u[i][N];
     fileOutput << "\n";
-    int tCount = 1;
-    for (int i = 0; i < uIdx; i++) {
-        fileOutput << tCount << ",";
-        double *row = u[i];
-        for (int j = 0; j <= N; j++) {
-            if (j == N) {
-                fileOutput << row[j];
-            } else
-                fileOutput << row[j] << ",";
-        }
-        tCount++;
-        fileOutput << "\n";
-    }
-
-    delete[] y1;
-    delete[] y2;
-    delete[] y3;
-    delete[] u;
-    return 0;
+  }
+  fileOutput.close();
+  return 0;
 }
